a762dc485dcb2de8387e609a6be63e5544258c09
default <srinathts94@gmail.com>
2024-10-03 16:40:10 +0000

    added photos and logo

--- .evershop/build/frontStore/notFound/client/entry.jsx
+++ .evershop/build/frontStore/notFound/client/entry.jsx
@@ -9,67 +9,67 @@ Area.defaultProps.components = {
     '137dcfd8bf7950dd6201684efdcb40a1': {
       id: '137dcfd8bf7950dd6201684efdcb40a1',
       sortOrder: 5,
-      component: require('/home/belvinnadar527/Fireworks/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/all/SearchBox.jsx')
+      component: require('/home/srinathts94/t-shirt/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/all/SearchBox.jsx')
     },
     '5248a628c0ba0eb2edc726fbc70778f8': {
       id: '5248a628c0ba0eb2edc726fbc70778f8',
       sortOrder: 10,
-      component: require('/home/belvinnadar527/Fireworks/node_modules/@evershop/evershop/src/modules/checkout/pages/frontStore/all/MiniCart.jsx')
+      component: require('/home/srinathts94/t-shirt/node_modules/@evershop/evershop/src/modules/checkout/pages/frontStore/all/MiniCart.jsx')
     },
     fef4b1d9ae2122930f9c33edbb982d6e: {
       id: 'fef4b1d9ae2122930f9c33edbb982d6e',
       sortOrder: 30,
-      component: require('/home/belvinnadar527/Fireworks/node_modules/@evershop/evershop/src/modules/customer/pages/frontStore/all/UserIcon.jsx')
+      component: require('/home/srinathts94/t-shirt/node_modules/@evershop/evershop/src/modules/customer/pages/frontStore/all/UserIcon.jsx')
     }
   },
   content: {
     '45515ab09f9ce8f561fc40ca4ffb7b2f': {
       id: '45515ab09f9ce8f561fc40ca4ffb7b2f',
       sortOrder: 0,
-      component: require('/home/belvinnadar527/Fireworks/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Breadcrumb.jsx')
+      component: require('/home/srinathts94/t-shirt/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Breadcrumb.jsx')
     },
     '203a19f62a10f9c6b049edff2c63595e': {
       id: '203a19f62a10f9c6b049edff2c63595e',
       sortOrder: 10,
-      component: require('/home/belvinnadar527/Fireworks/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/notFound/NotFound.jsx')
+      component: require('/home/srinathts94/t-shirt/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/notFound/NotFound.jsx')
     }
   },
   footer: {
     '03ce62a99b643c53cea33f6fc2c573f5': {
       id: '03ce62a99b643c53cea33f6fc2c573f5',
       sortOrder: 10,
-      component: require('/home/belvinnadar527/Fireworks/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Footer.jsx')
+      component: require('/home/srinathts94/t-shirt/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Footer.jsx')
     }
   },
   head: {
     '170528765f0295e4db900fd92ea387bc': {
       id: '170528765f0295e4db900fd92ea387bc',
       sortOrder: 5,
-      component: require('/home/belvinnadar527/Fireworks/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/HeadTags.jsx')
+      component: require('/home/srinathts94/t-shirt/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/HeadTags.jsx')
     },
     '52eeddcfff6fbe3b853ba32a42a93e82': {
       id: '52eeddcfff6fbe3b853ba32a42a93e82',
       sortOrder: 1,
-      component: require('/home/belvinnadar527/Fireworks/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/notFound/Meta.jsx')
+      component: require('/home/srinathts94/t-shirt/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/notFound/Meta.jsx')
     }
   },
   body: {
     '2aa0bf53e05360bd37f2dd0982998263': {
       id: '2aa0bf53e05360bd37f2dd0982998263',
       sortOrder: 1,
-      component: require('/home/belvinnadar527/Fireworks/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Layout.jsx')
+      component: require('/home/srinathts94/t-shirt/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Layout.jsx')
     },
     '18bf66723f1e3394e1ebb5dce01e8097': {
       id: '18bf66723f1e3394e1ebb5dce01e8097',
       sortOrder: 10,
-      component: require('/home/belvinnadar527/Fireworks/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Notification.jsx')
+      component: require('/home/srinathts94/t-shirt/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Notification.jsx')
     }
   },
   header: {
     a20b83833c6ae10ded5077b0943cfef5: {
       id: 'a20b83833c6ae10ded5077b0943cfef5',
       sortOrder: 10,
-      component: require('/home/belvinnadar527/Fireworks/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Logo.jsx')
+      component: require('/home/srinathts94/t-shirt/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Logo.jsx')
     }
   },
   '*': {
